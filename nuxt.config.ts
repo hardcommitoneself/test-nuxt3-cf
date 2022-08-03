5fc958631f36607eabc828699d9ddb9b699ed143
hardcommitoneself <hardcommitoneself@gmail.com>
2022-08-02 20:19:07 -0700

    Fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    "nitro": {
+        "output": {
+            dir: 'output',
+            serverDir: 'output/server',
+            publicDir: 'output/public'
+        }
+    },
+    "target": "static"
 })
